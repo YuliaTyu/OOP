1fb508a3be86fb7822c4bba9629e1623f8e93aff
Юля <@DESKTOP-LDN2BMM>
2025-06-28 00:48:22 +0300

    String

---
+++ String/main.cpp
@@ -0,0 +1,101 @@
+﻿#include<iostream>
+using namespace std;
+
+class String
+{
+	int size;  //размер строки в байтах 
+	char* str; //указатель на строку в динамической памяти 
+public:
+	int get_size()const
+	{
+		return size;
+	}
+	const char* get_str() const // гарантия что на месте вызова не изменится значение
+	{
+		return str;
+	}
+
+	//конструкторы
+
+	explicit String(int size = 80)
+	{
+		this->size = size;
+		this->str = new char[size] {};
+		cout << "DefaultConstruction\t" << this << endl;
+	}
+	String(const char str[])
+	{
+		while(str[size++]);
+			this->str = new char[size] {};
+			for (int i = 0; str[i]; i++)
+			this->str[i] = str[i];
+		cout << "Constuctor\t\t" << this << endl;
+	}
+
+	//конструктор копирования DeepCopy - ПОБИТОВОЕ КОПИРОВАНИЕ - т е выделять дин память под объект и побитово
+	// поэлементно копировать содержимое динам памяти из существующего объекта в создаваемый
+	String(const String& other)
+	{
+		this->size = other.size;
+		this->str = new char[size] {};
+		for (int i = 0; i < size; i++)this->str[i] = other.str[i];
+		cout << "CopyConstructor\t" << this << endl;
+	}
+
+	~String()
+	{
+		delete[] str;
+		this->str = nullptr;
+		this->size = 0;
+		cout << "Destructor\t\t" << this << endl;
+	}
+
+	//оператор присваивания
+	String& operator=(const String& other)
+	{
+		if (this == &other)return *this;
+		delete[] this->str;
+		this->size = other.size;
+		this->str = new char[size] {};
+		for (int i = 0; i < size; i++)this->str[i] = other.str[i];
+		cout << "CopyAssignment\t" << this << endl;
+		return *this;
+	}
+
+	//методы 
+	void info() const
+	{
+		cout << "Size\t" << size << endl;
+		cout << "Str\t" << str << endl;
+
+	}
+
+};
+
+
+std::ostream& operator<<(std::ostream& os, const String& obj)
+{
+	return os << obj.get_str();
+}
+
+void main()
+{
+	setlocale(LC_ALL, "");
+
+	String str1 (5);//explicit-конструктор можно вызвать при помощи круглых скобок
+	str1.info();
+	cout << str1 << endl;
+	str1.info();
+
+	String str2 = "Hello";
+	str2 = str2;
+	cout << str2 << endl;
+
+	String str3 = str2;
+	cout << str3 << endl;
+
+	String str4;
+	str4 = str3;
+	cout << str4 << endl;
+
+}
